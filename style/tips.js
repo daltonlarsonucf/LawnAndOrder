a153f63dd2eb03bd29e262cbef11484b902bfe2c
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-03-17 16:20:50 -0400

    dropdown update

---
+++ style/tips.js
@@ -0,0 +1,39 @@
+document.getElementById("houseplants-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("houseplants-dropdown").classList.toggle("show");
+}
+
+document.getElementById("herbs-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("herbs-dropdown").classList.toggle("show");
+}
+
+document.getElementById("veggies-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("veggies-dropdown").classList.toggle("show");
+}
+
+document.getElementById("fruits-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("fruits-dropdown").classList.toggle("show");
+}
+
+document.getElementById("flowers-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("flowers-dropdown").classList.toggle("show");
+}
+
+document.getElementById("growing-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("growing-dropdown").classList.toggle("show");
+}
+
+document.getElementById("kinds-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("kinds-dropdown").classList.toggle("show");
+}
+
+document.getElementById("started-button").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("started-dropdown").classList.toggle("show");
+}
