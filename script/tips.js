777b9c3d437ae1a56d3f51fb3306c97c491a23b6
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-03-17 16:29:29 -0400

    Update tips.js

--- script/tips.js
+++ script/tips.js
@@ -1,39 +1,39 @@
-document.getElementById("houseplants-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("houseplants-button").onclick = function() {Houseplants()};
+function Houseplants() {
   document.getElementById("houseplants-dropdown").classList.toggle("show");
 }
 
-document.getElementById("herbs-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("herbs-button").onclick = function() {Herbs()};
+function Herbs() {
   document.getElementById("herbs-dropdown").classList.toggle("show");
 }
 
-document.getElementById("veggies-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("veggies-button").onclick = function() {Veggies()};
+function Veggies() {
   document.getElementById("veggies-dropdown").classList.toggle("show");
 }
 
-document.getElementById("fruits-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("fruits-button").onclick = function() {Fruits()};
+function Fruits() {
   document.getElementById("fruits-dropdown").classList.toggle("show");
 }
 
-document.getElementById("flowers-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("flowers-button").onclick = function() {Flowers()};
+function Flowers() {
   document.getElementById("flowers-dropdown").classList.toggle("show");
 }
 
-document.getElementById("growing-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("growing-button").onclick = function() {Growing()};
+function Growing() {
   document.getElementById("growing-dropdown").classList.toggle("show");
 }
 
-document.getElementById("kinds-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("kinds-button").onclick = function() {Kinds()};
+function Kinds() {
   document.getElementById("kinds-dropdown").classList.toggle("show");
 }
 
-document.getElementById("started-button").onclick = function() {myFunction()};
-function myFunction() {
+document.getElementById("started-button").onclick = function() {Started()};
+function Started() {
   document.getElementById("started-dropdown").classList.toggle("show");
 }
