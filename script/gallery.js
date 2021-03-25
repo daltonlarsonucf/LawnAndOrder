63b54bea7dee36ec9c3cb268ce0e64acfb637545
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-03-24 23:15:02 -0400

    Modal

--- script/gallery.js
+++ script/gallery.js
@@ -1,23 +1,17 @@
-// Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var open = document.getElementById("openModal");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+var modal = document.getElementById("nothingModal");
+var open = document.getElementById("openNothing");
+var span = document.getElementsByClassName("close")[0];
+
 open.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
