18c8f45f41ec05af615dbf56fc69ba7de6fe50d6
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-03-25 00:24:00 -0400

    Modal

--- script/gallery.js
+++ script/gallery.js
@@ -8,7 +8,7 @@ open1.onclick = function() {
 span1.onclick = function() {
   modal1.style.display = "none";
 }
-window.onclick = function(event) {
+window.onclick = function(event1) {
   if (event.target == modal1) {
     modal1.style.display = "none";
   }
@@ -24,8 +24,72 @@ open2.onclick = function() {
 span2.onclick = function() {
   modal2.style.display = "none";
 }
-window.onclick = function(event) {
+window.onclick = function(event2) {
   if (event.target == modal2) {
     modal2.style.display = "none";
+  }
+}
+
+var modal3 = document.getElementById("gardenModal");
+var open3 = document.getElementById("openGarden");
+var span3 = document.getElementsByClassName("close")[2];
+
+open3.onclick = function() {
+  modal3.style.display = "block";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+window.onclick = function(event3) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+var modal4 = document.getElementById("pottingModal");
+var open4 = document.getElementById("openPotting");
+var span4 = document.getElementsByClassName("close")[3];
+
+open4.onclick = function() {
+  modal4.style.display = "block";
+}
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+window.onclick = function(event4) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+
+var modal5 = document.getElementById("eggplantModal");
+var open5 = document.getElementById("openEggplant");
+var span5 = document.getElementsByClassName("close")[4];
+
+open5.onclick = function() {
+  modal5.style.display = "block";
+}
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+window.onclick = function(event5) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
+
+var modal6 = document.getElementById("butterflyModal");
+var open6 = document.getElementById("openButterfly");
+var span6 = document.getElementsByClassName("close")[5];
+
+open6.onclick = function() {
+  modal6.style.display = "block";
+}
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+window.onclick = function(event6) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
   }
 }
