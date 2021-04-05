f1eab346acaef0c04d56a2b11681ecb751d46cbb
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-04-05 15:39:16 -0400

    Update

--- script/gallery.js
+++ script/gallery.js
@@ -14,8 +14,8 @@ window.onclick = function(event) {
   }
 }
 
-var modal2 = document.getElementById("nothingModal");
-var open2 = document.getElementById("openNothing");
+var modal2 = document.getElementById("tomatoesModal");
+var open2 = document.getElementById("openTomatoes");
 var span2 = document.getElementsByClassName("close")[1];
 
 open2.onclick = function() {
@@ -91,5 +91,101 @@ span6.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal6) {
     modal6.style.display = "none";
+  }
+}
+
+var modal7 = document.getElementById("strawberryModal");
+var open7 = document.getElementById("openStrawberry");
+var span7 = document.getElementsByClassName("close")[6];
+
+open7.onclick = function() {
+  modal7.style.display = "block";
+}
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal7) {
+    modal7.style.display = "none";
+  }
+}
+
+var modal8 = document.getElementById("sowingModal");
+var open8 = document.getElementById("openSowing");
+var span8 = document.getElementsByClassName("close")[7];
+
+open8.onclick = function() {
+  modal8.style.display = "block";
+}
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+}
+
+var modal9 = document.getElementById("sittingModal");
+var open9 = document.getElementById("openSitting");
+var span9 = document.getElementsByClassName("close")[8];
+
+open9.onclick = function() {
+  modal9.style.display = "block";
+}
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal9) {
+    modal9.style.display = "none";
+  }
+}
+
+var modal10 = document.getElementById("cuttingModal");
+var open10 = document.getElementById("openCutting");
+var span10 = document.getElementsByClassName("close")[9];
+
+open10.onclick = function() {
+  modal10.style.display = "block";
+}
+span10.onclick = function() {
+  modal10.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal10) {
+    modal10.style.display = "none";
+  }
+}
+
+var modal11 = document.getElementById("lettuceModal");
+var open11 = document.getElementById("openLettuce");
+var span11 = document.getElementsByClassName("close")[10];
+
+open11.onclick = function() {
+  modal11.style.display = "block";
+}
+span11.onclick = function() {
+  modal11.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal11) {
+    modal11.style.display = "none";
+  }
+}
+
+var modal12 = document.getElementById("potsModal");
+var open12 = document.getElementById("openPots");
+var span12 = document.getElementsByClassName("close")[11];
+
+open12.onclick = function() {
+  modal12.style.display = "block";
+}
+span12.onclick = function() {
+  modal12.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal12) {
+    modal12.style.display = "none";
   }
 }
