2926df369277707c56036461d98ffe647eff1dc3
daltonlarsonucf <59659050+daltonlarsonucf@users.noreply.github.com>
2021-04-10 14:32:24 -0400

    Update

--- script/events.js
+++ script/events.js
@@ -1,24 +1,109 @@
-var going = document.getElementById("valentine-going");
-var notgoing = document.getElementById("valentine-notgoing");
-
 function ValentineGoing() {
-  if(notgoing.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
-    going.src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
-    if(going.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
-      going.src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
-    }else if(going.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
-      going.src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
+  if(document.getElementById("valentine-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+    document.getElementById("valentine-going").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    if(document.getElementById("valentine-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
+      document.getElementById("valentine-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    }else if(document.getElementById("valentine-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+      document.getElementById("valentine-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
     }
   }
 }
 
 function ValentineNotGoing() {
-  if(going.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
-    notgoing.src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
-    if(notgoing.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
-      notgoing.src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
-    }else if(notgoing.src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
-      notgoing.src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
+  if(document.getElementById("valentine-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+    document.getElementById("valentine-notgoing").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
+    if(document.getElementById("valentine-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
+      document.getElementById("valentine-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
+    }else if(document.getElementById("valentine-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+      document.getElementById("valentine-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
+    }
+  }
+}
+
+function PieGoing() {
+  if(document.getElementById("pie-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+    document.getElementById("pie-going").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    if(document.getElementById("pie-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
+      document.getElementById("pie-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    }else if(document.getElementById("pie-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+      document.getElementById("pie-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
+    }
+  }
+}
+
+function PieNotGoing() {
+  if(document.getElementById("pie-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+    document.getElementById("pie-notgoing").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
+    if(document.getElementById("pie-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
+      document.getElementById("pie-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
+    }else if(document.getElementById("pie-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+      document.getElementById("pie-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
+    }
+  }
+}
+
+function HerbsGoing() {
+  if(document.getElementById("herbs-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+    document.getElementById("herbs-going").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    if(document.getElementById("herbs-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
+      document.getElementById("herbs-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    }else if(document.getElementById("herbs-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+      document.getElementById("herbs-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
+    }
+  }
+}
+
+function HerbsNotGoing() {
+  if(document.getElementById("herbs-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+    document.getElementById("herbs-notgoing").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
+    if(document.getElementById("herbs-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
+      document.getElementById("herbs-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
+    }else if(document.getElementById("herbs-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+      document.getElementById("herbs-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
+    }
+  }
+}
+
+function StarwarsGoing() {
+  if(document.getElementById("starwars-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+    document.getElementById("starwars-going").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    if(document.getElementById("starwars-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
+      document.getElementById("starwars-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    }else if(document.getElementById("starwars-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+      document.getElementById("starwars-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
+    }
+  }
+}
+
+function StarwarsNotGoing() {
+  if(document.getElementById("starwars-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+    document.getElementById("starwars-notgoing").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
+    if(document.getElementById("starwars-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
+      document.getElementById("starwars-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
+    }else if(document.getElementById("starwars-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+      document.getElementById("starwars-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
+    }
+  }
+}
+
+function VictoryGoing() {
+  if(document.getElementById("victory-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+    document.getElementById("victory-going").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    if(document.getElementById("victory-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png"){
+      document.getElementById("victory-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png";
+    }else if(document.getElementById("victory-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+      document.getElementById("victory-going").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going1.png";
+    }
+  }
+}
+
+function VictoryNotGoing() {
+  if(document.getElementById("victory-going").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/going2.png"){
+    document.getElementById("victory-notgoing").src != "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png";
+    if(document.getElementById("victory-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png"){
+      document.getElementById("victory-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"
+    }else if(document.getElementById("victory-notgoing").src == "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing2.png"){
+      document.getElementById("victory-notgoing").src = "file:///Users/daltonlarson/Documents/GitHub/LawnAndOrder/images/notgoing1.png";
     }
   }
 }
